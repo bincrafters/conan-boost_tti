0820f28ea4607d675e8c208fff0eec6be9d6c183
Jerry W <jerrywiltse@gmail.com>
2018-02-17 09:39:15 -0500

    merge from testing

--- conanfile.py
+++ conanfile.py
@@ -17,13 +17,13 @@ class BoostTtiConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.65.1@bincrafters/testing",
-        "boost_config/1.65.1@bincrafters/testing",
-        "boost_function_types/1.65.1@bincrafters/testing",
-        "boost_mpl/1.65.1@bincrafters/testing",
-        "boost_optional/1.65.1@bincrafters/testing",
-        "boost_preprocessor/1.65.1@bincrafters/testing",
-        "boost_type_traits/1.65.1@bincrafters/testing"
+        "boost_package_tools/1.65.1@bincrafters/stable",
+        "boost_config/1.65.1@bincrafters/stable",
+        "boost_function_types/1.65.1@bincrafters/stable",
+        "boost_mpl/1.65.1@bincrafters/stable",
+        "boost_optional/1.65.1@bincrafters/stable",
+        "boost_preprocessor/1.65.1@bincrafters/stable",
+        "boost_type_traits/1.65.1@bincrafters/stable"
     )
 
     # BEGIN
@@ -31,7 +31,7 @@ class BoostTtiConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_65_1"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.65.1@bincrafters/testing"
+    build_requires = "boost_generator/1.65.1@bincrafters/stable"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
