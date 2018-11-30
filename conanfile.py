c9f99de99394e8c01d45260d33562e4ba119fc9d
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:13 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostTtiConan(base.BoostBaseConan):
     name = "boost_tti"
@@ -18,4 +18,3 @@ class BoostTtiConan(base.BoostBaseConan):
         "boost_preprocessor",
         "boost_type_traits"
     ]
-
