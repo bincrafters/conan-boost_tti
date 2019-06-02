3fe83a16fc25866dbb44dde86f2ec574b74bb4dc
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:09 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,18 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostTtiConan(base.BoostBaseConan):
     name = "boost_tti"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_tti"
-    lib_short_names = ["tti"]
-    header_only_libs = ["tti"]
-    b2_requires = [
-        "boost_config",
-        "boost_function_types",
-        "boost_mpl",
-        "boost_preprocessor",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
